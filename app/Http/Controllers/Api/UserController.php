01151a406bc59020891c1d08531c8dbc0637e8a5
SenrinSenrin <senrin.sim.webprograming@gmail.com>
2025-10-18 14:05:02 +0700

    add api code to testing in production

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getUsers(){
+        return 'Hello Bro!';
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\UserController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API working']);
+});
+
+Route::prefix('users')->group(function (){ 
+    Route::get('/', [UserController::class, 'getUsers']);
 });
